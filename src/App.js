487f8b2885c7082e53a0274de84dc666038cd37d
Jefferson Lucio da Silva <jeferson.silva@tora.com.br>
2020-02-22 23:16:12 -0300

    two commit into aplication

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './index.css'
+
+
+
+export default class App extends React.Component {
+  state = {
+    text: '',
+    todos: []
+    }
+
+  componentDidMount() {
+    axios.get(`http://localhost:3333/todos`)
+      .then(res => {
+        const todos = res.data;
+        this.setState({ todos });
+      })
+  }
+
+  handleChange = event => {
+    this.setState({ text: event.target.value });
+    
+  }
+
+  limparForm = e => {
+    this.setState({
+		   text: '',
+      erros: ''
+    });
+  }
+
+  handleSubmit = event => {
+    this.limparForm() 
+    event.preventDefault();
+    axios.post(`http://localhost:3333/todos`,{ text: this.state.text})
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+        this.limparForm()
+       this.setState({ todos: [...this.state.todos, res.data]});
+      })
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>TO DO LIST WEEK 1 OMINISTACK</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <TextField type="text" name="text"  className="Input"
+            helperText="insert new todolist ."
+            variant="filled"
+            onChange={this.handleChange} />
+          </label>
+          <Button type="submit" variant="contained" id="button">Adicionar</Button>
+        </form>
+        <Card id="Card">
+        { this.state.todos.map(todos => <li>{todos.text}</li>)}
+      </Card>
+      </div>
+    )
+  }
 }
-
-export default App;
